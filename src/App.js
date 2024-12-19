6b9b3325f6676ccb3d0dc3a52816e2721d8fb98f
kavincode0 <kav.sharma0291@gmail.com>
2024-12-20 00:38:48 +0530

    frontend base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import logo from './components/logo.png'
+import Navbar from './components/NavBar'
+import { GiHamburgerMenu } from "react-icons/gi";
 import './App.css';
 
 function App() {
+
+  const [ showNav, setShowNav ] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <Router>
+      <div className="App">
+      
+      <header>
+        <div>
+          <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        </div>
+          <img src={logo} alt="Logo" className='logo'/>
       </header>
+      
+      <Navbar show={showNav}/>
+          
+      <div className='main'>
+          <Routes>
+            <Route path='/home' exact={true} element={<Home />}/>
+            <Route path='/about' exact={true} element={<About />}/>
+            <Route path='/contact' exact={true} element={<Contact/>}/>
+          </Routes>
+      </div>
     </div>
+      </Router>
+    </>
   );
 }
 

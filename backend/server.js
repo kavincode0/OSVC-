25ddfe69e830ec4a2f1dada26af639dee37aa03a
kavincode0 <kav.sharma0291@gmail.com>
2024-12-23 02:39:06 +0530

    server created

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+const vulnerabilitiesRoute = require('./routes/vulnerabilities');
+app.use('/api/v1/vulnerabilities', vulnerabilitiesRoute);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
